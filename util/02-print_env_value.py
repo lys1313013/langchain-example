50fbabf723a68c85c4fcf56068d953cfe0ece00b
lys1313013 <lys1313013@gmail.com>
2025-09-28 19:34:08 +0800

    refactor: 抽取打印环境变量方法

--- util/02-print_env_value.py
+++ util/02-print_env_value.py
@@ -1,20 +1,25 @@
 import os
 
+def print_env_value(env_name):
+    """打印指定环境变量的值"""
+    print(f"{env_name}: {os.getenv(env_name)}")
+
 # 打印经常使用的一些环境变量值
 
-print(f"OPENAI_API_KEY: {os.getenv('OPENAI_API_KEY')}")
-print(f"OPENAI_API_BASE: {os.getenv('OPENAI_API_BASE')}")
+# OPENAI
+print_env_value("OPENAI_API_KEY")
+print_env_value("OPENAI_API_BASE")
 
-print(f"LANGCHAIN_TRACING_V2: {os.getenv('LANGCHAIN_TRACING_V2')}")
-print(f"LANGCHAIN_API_KEY: {os.getenv('LANGCHAIN_API_KEY')}")
+print_env_value("LANGCHAIN_TRACING_V2")
+print_env_value("LANGCHAIN_API_KEY")
 
 # Tavily api key
-print(f"TAVILY_API_KEY: {os.getenv('TAVILY_API_KEY')}")
+print_env_value("TAVILY_API_KEY")
 
 # DeepSeek api key
-print(f"DEEPSEEK_API_KEY", os.getenv("DEEPSEEK_API_KEY"))
+print_env_value("DEEPSEEK_API_KEY")
 # 阿里百炼 api key
-print(f"DASHSCOPE_API_KEY: {os.getenv('DASHSCOPE_API_KEY')}")
+print_env_value("DASHSCOPE_API_KEY")
 
 # 查询天气api key 对应网站：https://openweathermap.org/api
-print(f"OPENWEATHER_API_KEY: {os.getenv('OPENWEATHER_API_KEY')}")
+print_env_value("OPENWEATHER_API_KEY")
