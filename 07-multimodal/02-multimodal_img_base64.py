3c7be5bbb03e2cd6a3c643e3c3073651fd2a38a0
lys1313013 <lys1313013@gmail.com>
2026-03-02 21:01:08 +0800

    feat: 适配 远程图片和本地图片

--- 07-multimodal/02-multimodal_img_base64.py
+++ 07-multimodal/02-multimodal_img_base64.py
@@ -4,12 +4,24 @@ import os
 
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-# 用httpx,获取图片的base64编码
 import httpx
 
-image_url = "https://img0.baidu.com/it/u=4036329736,2858248034&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=1200"
-# 将图片二进制数据转换为base64编码
-image_data = base64.b64encode(httpx.get(image_url).content).decode("utf-8")
+
+def get_image_data(image_path: str) -> str:
+    """
+    适配 远程图片和本地图片
+    """
+    if image_path.startswith("http://") or image_path.startswith("https://"):
+        return base64.b64encode(httpx.get(image_path).content).decode("utf-8")
+    else:
+        return base64.b64encode(open(image_path, "rb").read()).decode("utf-8")
+
+
+image_url = "/Users/hurry/Documents/coding/projects/llm_preojects/Qwen3-VL-Embedding/data/evaluation/mmeb_v2/image-tasks/MSCOCO_t2i/COCO_val2014_000000181859.jpg"
+# 示例：HTTP URL 方式
+# image_url = "https://img0.baidu.com/it/u=4036329736,2858248034&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=1200"
+
+image_data = get_image_data(image_url)
 chatLLM = ChatOpenAI(
     api_key=os.getenv("DASHSCOPE_API_KEY"),
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
